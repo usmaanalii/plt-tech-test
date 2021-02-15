6341d40d8786caf5907aabfa871dc72fca5ea31a
Usmaan Ali <usmaan.ali@radiuspaymentsolutions.com>
2021-02-15 18:51:52 +0000

    NO-JIRA: Refactor the method
    - prevents an additional iteration through the transactions array
    - compute the qty outside of the object construction for readability

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,11 @@ import { stock } from './stock';
 import { transactions } from './transactions';
 
 export const app = (sku: string): Promise<{ sku: string, qty: number }> => new Promise((resolve, reject) => {
-  if (!transactions.map(({ sku }) => sku).includes(sku)) reject('This sku does not exist');
+  const selectedTransaction = transactions.find(({ sku: transactionSku }) => transactionSku === sku);
 
-  const qty = stock.map(({ sku }) => sku).includes(sku) ? transactions.find(({ sku }) => sku === sku).qty : 0;
+  if (!selectedTransaction) reject('This sku does not exist');
+
+  const qty = stock.map(({ sku }) => sku).includes(sku) ? selectedTransaction.qty : 0;
 
   return resolve({ sku, qty });
 });
