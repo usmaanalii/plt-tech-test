58791f515867ecec18f1be0aeb501c274f1aa3ff
Usmaan Ali <usmaan.ali@radiuspaymentsolutions.com>
2021-02-19 10:10:21 +0000

    NO-JIRA: Rewrite tests based on feedback

--- src/app.spec.ts
+++ src/app.spec.ts
@@ -1,98 +1,374 @@
-const mockedInStockSku = 'some mocked in stock sku';
-const mockedOutOfStockSku = 'some mocked out of stock sku'
-const mockedIncorrectSku = 'some mocked incorrect sku';
+const generateMockedStockItem = (sku, stock) => ({ sku, stock });
+const generateMockedTransactionItem = (sku, type, qty) => ({ sku, type, qty });
 
-const mockedStockItemSku = mockedInStockSku;
-const mockFirstStockItem = { sku: mockedStockItemSku };
-const mockedStock = [ mockFirstStockItem ];
-
-const mockedFirstTransactionItemSku = mockedInStockSku;
-const mockedFirstTransactionItemType = 'some mocked first transaction item type';
-const mockedFirstTransactionItemQty = 'some mocked first transaction item quantity';
-
-const mockedSecondTransactionItemSku = mockedOutOfStockSku;
-const mockedSecondTransactionItemType = 'some mocked second transaction item type';
-const mockedSecondTransactionItemQty = 'some mocked second transaction item quantity';
-
-const mockedFirstTransaction = {
-  sku: mockedFirstTransactionItemSku,
-  type: mockedFirstTransactionItemType,
-  qty: mockedFirstTransactionItemQty
-};
-
-const mockedSecondTransaction = {
-  sku: mockedSecondTransactionItemSku,
-  type: mockedSecondTransactionItemType,
-  qty: mockedSecondTransactionItemQty
-};
-
-const mockedTransactions = [
-  mockedFirstTransaction,
-  mockedSecondTransaction
-];
-
-const expectedError = 'This sku does not exist';
+const mockImports = (stock, transactions) => {
+  jest.doMock('./stock', () => ({ stock }));
+  jest.doMock('./transactions', () => ({ transactions }));
+}
 
 describe('Given the app', () => {
   let app;
 
-  beforeEach(() => {
-    jest.doMock('./stock', () => ({
-      stock: mockedStock
-    }));
-
-    jest.doMock('./transactions', () => ({
-      transactions: mockedTransactions
-    }));
-  });
-
   describe('When imported as a node module', () => {
     beforeEach(() => {
-      ({ app } = require('./app'));
+      jest.resetModules();
     });
 
     describe('And when calling app with an sku that does NOT exist', () => {
       let actualError;
 
-      beforeEach(() => app(mockedIncorrectSku).catch(error =>actualError = error));
+      const mockedStock = [
+        generateMockedStockItem('some mocked stock item sku', 'some mocked stock item stock')
+      ];
+
+      const mockedTransactions = [
+        generateMockedTransactionItem(
+          'some mocked transaction item sku',
+          'some mocked transaction item type',
+          'some mocked transaction item qty'
+          )
+      ];
+
+      const mockedIncorrectSku = 'some mocked incorrect sku';
+
+      const expectedError = 'This sku does not exist';
+
+      beforeEach(() => {
+        mockImports(mockedStock, mockedTransactions);
+
+        ({ app } = require('./app'));
+
+        app(mockedIncorrectSku).catch(error => actualError = error)
+      });
 
       it('Then should return the expected error', () => {
         expect(actualError).toBe(expectedError)
       });
     });
 
-    describe('And when calling app with an sku that does exist but is NOT in stock', () => {
+    describe('And when calling app with an sku that has at least one transaction but is NOT in stock', () => {
       let actualResponse;
 
+      const mockTransactionItemSku = 'some mocked transaction item sku';
+
+      const mockedStock = [];
+
+      const mockedTransactions = [
+        generateMockedTransactionItem(
+          mockTransactionItemSku,
+          'some mocked transaction item type',
+          'some mocked transaction item qty'
+          )
+      ];
+
       const expectedResponse = {
-        sku: mockedOutOfStockSku,
+        sku: mockTransactionItemSku,
         qty: 0
       };
 
-      beforeEach(() => app(mockedOutOfStockSku).then(response => actualResponse = response));
+      beforeEach(() => {
+        mockImports(mockedStock, mockedTransactions);
 
-      it('Then should return the expected response', () => {
+        ({ app } = require('./app'));
+
+        app(mockTransactionItemSku).then(response => actualResponse = response)
+      });
+
+      it('Then should return the expected sku with a 0 qty', () => {
           expect(actualResponse).toEqual(expectedResponse)
         });
     });
 
-    describe('And when calling app with an sku that does exist in stock', () => {
+    describe('And when calling app with an sku that has a single "order" transaction AND NO "refund" transactions', () => {
       let actualResponse;
 
+      const mockedSku = 'some mocked sku' ;
+      const mockedStockItemStock = 100;
+      const mockedStock = [
+        generateMockedStockItem(mockedSku, mockedStockItemStock)
+      ];
+
+      const mockedTransactionQty = 10;
+      const mockedTransactions = [
+        generateMockedTransactionItem(mockedSku, 'order', mockedTransactionQty)
+      ];
+
+      const expectedQty = mockedStockItemStock - mockedTransactionQty;
+
       const expectedResponse = {
-        sku: mockedInStockSku,
-        qty: mockedFirstTransaction.qty
+        sku: mockedSku,
+        qty: expectedQty
       };
 
-      beforeEach(() => app(mockedInStockSku).then(response => actualResponse = response));
+      beforeEach(() => {
+        mockImports(mockedStock, mockedTransactions);
 
-      it('Then should return the expected response', () => {
+        ({ app } = require('./app'));
+
+        app(mockedSku).then(response => actualResponse = response)
+      });
+
+      it('Then should return the sku with the correctly calculated qty', () => {
           expect(actualResponse).toEqual(expectedResponse)
         });
     });
-  });
 
-  afterEach(() => {
-    jest.resetModules();
+    describe('And when calling app with an sku that has NO "order" transactions AND a single "refund" transaction', () => {
+      let actualResponse;
+
+      const mockedSku = 'some mocked sku' ;
+      const mockedStockItemStock = 100;
+      const mockedStock = [
+        generateMockedStockItem(mockedSku, mockedStockItemStock)
+      ];
+
+      const mockedTransactionQty = 10;
+      const mockedTransactions = [
+        generateMockedTransactionItem(mockedSku, 'refund', mockedTransactionQty)
+      ];
+
+      const expectedQty = mockedStockItemStock + mockedTransactionQty;
+
+      const expectedResponse = {
+        sku: mockedSku,
+        qty: expectedQty
+      };
+
+      beforeEach(() => {
+        mockImports(mockedStock, mockedTransactions);
+
+        ({ app } = require('./app'));
+
+        app(mockedSku).then(response => actualResponse = response)
+      });
+
+      it('Then should return the sku with the correctly calculated qty', () => {
+          expect(actualResponse).toEqual(expectedResponse)
+        });
+    });
+
+    describe('And when calling app with an sku that has more than one "order" transaction and NO "refund" transactions', () => {
+      let actualResponse;
+
+      const mockedSku = 'some mocked sku' ;
+      const mockedStockItemStock = 100;
+      const mockedStock = [
+        generateMockedStockItem(mockedSku, mockedStockItemStock)
+      ];
+
+      const mockedFirstTransactionQty = 10;
+      const mockedSecondTransactionQty = 20;
+      const mockedTransactions = [
+        generateMockedTransactionItem(mockedSku, 'order', mockedFirstTransactionQty),
+        generateMockedTransactionItem(mockedSku, 'order', mockedSecondTransactionQty)
+      ];
+
+      const expectedQty = mockedStockItemStock - mockedFirstTransactionQty - mockedSecondTransactionQty;
+
+      const expectedResponse = {
+        sku: mockedSku,
+        qty: expectedQty
+      };
+
+      beforeEach(() => {
+        mockImports(mockedStock, mockedTransactions);
+
+        ({ app } = require('./app'));
+
+        app(mockedSku).then(response => actualResponse = response)
+      });
+
+      it('Then should return the sku with the correctly calculated qty', () => {
+          expect(actualResponse).toEqual(expectedResponse)
+        });
+    });
+
+    describe('And when calling app with an sku that has NO "order" transactions AND more than one "refund" transaction', () => {
+      let actualResponse;
+
+      const mockedSku = 'some mocked sku' ;
+      const mockedStockItemStock = 100;
+      const mockedStock = [
+        generateMockedStockItem(mockedSku, mockedStockItemStock)
+      ];
+
+      const mockedFirstTransactionQty = 10;
+      const mockedSecondTransactionQty = 20;
+      const mockedTransactions = [
+        generateMockedTransactionItem(mockedSku, 'refund', mockedFirstTransactionQty),
+        generateMockedTransactionItem(mockedSku, 'refund', mockedSecondTransactionQty)
+      ];
+
+      const expectedQty = mockedStockItemStock + mockedFirstTransactionQty + mockedSecondTransactionQty;
+
+      const expectedResponse = {
+        sku: mockedSku,
+        qty: expectedQty
+      };
+
+      beforeEach(() => {
+        mockImports(mockedStock, mockedTransactions);
+
+        ({ app } = require('./app'));
+
+        app(mockedSku).then(response => actualResponse = response)
+      });
+
+      it('Then should return the sku with the correctly calculated qty', () => {
+          expect(actualResponse).toEqual(expectedResponse)
+        });
+    });
+
+    describe('And when calling app with an sku that has a single "order" transaction AND a single "refund" transaction', () => {
+      let actualResponse;
+
+      const mockedSku = 'some mocked sku' ;
+      const mockedStockItemStock = 100;
+      const mockedStock = [
+        generateMockedStockItem(mockedSku, mockedStockItemStock)
+      ];
+
+      const mockedFirstTransactionQty = 10;
+      const mockedSecondTransactionQty = 20;
+      const mockedTransactions = [
+        generateMockedTransactionItem(mockedSku, 'order', mockedFirstTransactionQty),
+        generateMockedTransactionItem(mockedSku, 'refund', mockedSecondTransactionQty)
+      ];
+
+      const expectedQty = mockedStockItemStock - mockedFirstTransactionQty + mockedSecondTransactionQty;
+
+      const expectedResponse = {
+        sku: mockedSku,
+        qty: expectedQty
+      };
+
+      beforeEach(() => {
+        mockImports(mockedStock, mockedTransactions);
+
+        ({ app } = require('./app'));
+
+        app(mockedSku).then(response => actualResponse = response)
+      });
+
+      it('Then should return the sku with the correctly calculated qty', () => {
+          expect(actualResponse).toEqual(expectedResponse)
+        });
+    });
+
+    describe('And when calling app with an sku that has a single "order" AND more than one "refund" transaction', () => {
+      let actualResponse;
+
+      const mockedSku = 'some mocked sku' ;
+      const mockedStockItemStock = 100;
+      const mockedStock = [
+        generateMockedStockItem(mockedSku, mockedStockItemStock)
+      ];
+
+      const mockedFirstTransactionQty = 10;
+      const mockedSecondTransactionQty = 20;
+      const mockedThirdTransactionQty = 30;
+      const mockedTransactions = [
+        generateMockedTransactionItem(mockedSku, 'order', mockedFirstTransactionQty),
+        generateMockedTransactionItem(mockedSku, 'refund', mockedSecondTransactionQty),
+        generateMockedTransactionItem(mockedSku, 'refund', mockedThirdTransactionQty)
+      ];
+
+      const expectedQty = mockedStockItemStock - mockedFirstTransactionQty + mockedSecondTransactionQty + mockedThirdTransactionQty;
+
+      const expectedResponse = {
+        sku: mockedSku,
+        qty: expectedQty
+      };
+
+      beforeEach(() => {
+        mockImports(mockedStock, mockedTransactions);
+
+        ({ app } = require('./app'));
+
+        app(mockedSku).then(response => actualResponse = response)
+      });
+
+      it('Then should return the sku with the correctly calculated qty', () => {
+        expect(actualResponse).toEqual(expectedResponse)
+      });
+    });
+
+    describe('And when calling app with an sku that has a more than one "order" transaction AND a single "refund" transaction', () => {
+      let actualResponse;
+
+      const mockedSku = 'some mocked sku' ;
+      const mockedStockItemStock = 100;
+      const mockedStock = [
+        generateMockedStockItem(mockedSku, mockedStockItemStock)
+      ];
+
+      const mockedFirstTransactionQty = 10;
+      const mockedSecondTransactionQty = 20;
+      const mockedThirdTransactionQty = 30;
+      const mockedTransactions = [
+        generateMockedTransactionItem(mockedSku, 'refund', mockedFirstTransactionQty),
+        generateMockedTransactionItem(mockedSku, 'order', mockedSecondTransactionQty),
+        generateMockedTransactionItem(mockedSku, 'order', mockedThirdTransactionQty)
+      ];
+
+      const expectedQty = mockedStockItemStock + mockedFirstTransactionQty - mockedSecondTransactionQty - mockedThirdTransactionQty;
+
+      const expectedResponse = {
+        sku: mockedSku,
+        qty: expectedQty
+      };
+
+      beforeEach(() => {
+        mockImports(mockedStock, mockedTransactions);
+
+        ({ app } = require('./app'));
+
+        app(mockedSku).then(response => actualResponse = response)
+      });
+
+      it('Then should return the sku with the correctly calculated qty', () => {
+        expect(actualResponse).toEqual(expectedResponse)
+      });
+    });
+
+    describe('And when calling app with an sku that has a more than one "order" transaction AND more than one "refund" transaction', () => {
+      let actualResponse;
+
+      const mockedSku = 'some mocked sku' ;
+      const mockedStockItemStock = 100;
+      const mockedStock = [
+        generateMockedStockItem(mockedSku, mockedStockItemStock)
+      ];
+
+      const mockedFirstTransactionQty = 10;
+      const mockedSecondTransactionQty = 20;
+      const mockedThirdTransactionQty = 30;
+      const mockedFourthTransactionQty = 40;
+      const mockedTransactions = [
+        generateMockedTransactionItem(mockedSku, 'order', mockedFirstTransactionQty),
+        generateMockedTransactionItem(mockedSku, 'order', mockedSecondTransactionQty),
+        generateMockedTransactionItem(mockedSku, 'refund', mockedThirdTransactionQty),
+        generateMockedTransactionItem(mockedSku, 'refund', mockedFourthTransactionQty)
+      ];
+
+      const expectedQty = mockedStockItemStock - mockedFirstTransactionQty - mockedSecondTransactionQty + mockedThirdTransactionQty + mockedFourthTransactionQty;
+
+      const expectedResponse = {
+        sku: mockedSku,
+        qty: expectedQty
+      };
+
+      beforeEach(() => {
+        mockImports(mockedStock, mockedTransactions);
+
+        ({ app } = require('./app'));
+
+        app(mockedSku).then(response => actualResponse = response)
+      });
+
+      it('Then should return the sku with the correctly calculated qty', () => {
+        expect(actualResponse).toEqual(expectedResponse)
+      });
+    });
   });
 });
--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,31 @@ import { stock } from './stock';
 import { transactions } from './transactions';
 
 export const app = (sku: string): Promise<{ sku: string, qty: number }> => new Promise((resolve, reject) => {
-  const selectedTransaction = transactions.find(({ sku: transactionSku }) => transactionSku === sku);
+  const transaction = transactions.find(({ sku: transactionSku }) => transactionSku === sku);
+  const stockItem = stock.find(({ sku: stockItemSku }) => stockItemSku === sku)
 
-  if (!selectedTransaction) reject('This sku does not exist');
+  if (!transaction && !stockItem) reject('This sku does not exist');
 
-  const qty = stock.map(({ sku }) => sku).includes(sku) ? selectedTransaction.qty : 0;
+  if (transaction && !stockItem) resolve({ sku, qty: 0 });
+
+  const { stock: startingStock } = stockItem;
+
+  const qty = transactions.reduce((acc, { sku: transactionSku, type, qty }) => {
+    if (transactionSku === sku) {
+      switch (type){
+        case 'order':
+          acc -= qty;
+          break;
+        case 'refund':
+          acc += qty;
+          break
+        default:
+          break
+
+      }
+    }
+    return acc;
+  }, startingStock);
 
   return resolve({ sku, qty });
 });
